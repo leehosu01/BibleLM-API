294d672d14543c37145491001c774d575a2d5ace
map <leehosu01@naver.com>
2021-08-05 18:59:41 +0900

    clone 시점 바꿈

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from BibleLM import inference as model_inference
 app = Flask(__name__)
 CORS(app) 
 
-subprocess.run("git clone https://github.com/leehosu01/BibleLM.git /opt/app", shell = True)
 @app.route("/inference", methods=["GET","POST"])
 def inference():
     try:
@@ -20,7 +19,10 @@ def inference():
         request_cnt = int(params.get("samples", "1"))
         request_cnt = min(8, request_cnt)
         data = {}
-        data.update({'candidates': model_inference.inference(sentence, request_cnt, "/opt/app/BibleLM/Bible_model_ckpts") if request_cnt > 0 else []})
+        try:data.update({'candidates': model_inference.inference(sentence, request_cnt, "/opt/app/BibleLM/Bible_model_ckpts") if request_cnt > 0 else []})
+        except:
+            subprocess.run("git clone https://github.com/leehosu01/BibleLM.git /opt/app", shell = True)
+            data.update({'candidates': model_inference.inference(sentence, request_cnt, "/opt/app/BibleLM/Bible_model_ckpts") if request_cnt > 0 else []})
         data.update({"success": True})
     except Exception as e:
         data = {"success": False, 'error' : str(e)}
