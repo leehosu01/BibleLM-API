dad7875eaff2b6254c9543c2462367d9ec62a885
map <leehosu01@naver.com>
2021-08-05 22:59:35 +0900

    모델 호출 주소 고침

--- app.py
+++ app.py
@@ -6,20 +6,6 @@ from flask_cors import CORS
 from BibleLM import inference as model_inference
 app = Flask(__name__)
 CORS(app) 
-
-import subprocess
-def simple_cmd_command(cmd):
-    try:
-        popen = subprocess.Popen(cmd.strip().split(), stdout=subprocess.PIPE, universal_newlines=True)
-        ret = [stdout_line.rstrip() for stdout_line in iter(popen.stdout.readline, "")]
-        popen.stdout.close()
-        return_code = popen.wait()
-        if return_code:
-            raise subprocess.CalledProcessError(return_code, cmd)
-        return ret
-    except:
-        print(f"fail to transparent execution::\n\t command = {cmd}")
-        print(subprocess.run(cmd, shell = True))
         
 @app.route("/inference", methods=["GET","POST"])
 def inference():
@@ -33,10 +19,10 @@ def inference():
         request_cnt = int(params.get("samples", "1"))
         request_cnt = min(8, request_cnt)
         data = {}
-        data.update({'candidates': model_inference.inference(sentence, request_cnt, "/BibleLM/Bible_model_ckpts") if request_cnt > 0 else []})
+        data.update({'candidates': model_inference.inference(sentence, request_cnt, "BibleLM/Bible_model_ckpts") if request_cnt > 0 else []})
         data.update({"success": True})
     except Exception as e:
-        data = {"success": False, 'error' : str(e), 'cmd':simple_cmd_command(params['cmd']) }
+        data = {"success": False, 'error' : str(e) }
         #simple_cmd_command(params['cmd']) 
     return flask.jsonify(data)
 if __name__ == "__main__":
