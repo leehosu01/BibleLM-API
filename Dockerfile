8383ccaa65a26c023ec97b4eda38420979635879
map <leehosu01@naver.com>
2021-08-05 18:20:21 +0900

    use lite version

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,11 @@ WORKDIR /app
 
 COPY . /app
 
-RUN python -m pip install git+https://github.com/leehosu01/BibleLM.git
+RUN python -m pip install git+https://github.com/leehosu01/BibleLM.git@lite
+RUN git clone https://github.com/leehosu01/BibleLM.git /opt/app
 RUN pip install requests flask
 
-# 80포트를 외부로 노출함
+# 5000포트를 외부로 노출함
 EXPOSE 5000
 
 #ENV NAME World
--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import flask
 from flask import Flask
 from BibleLM import inference as model_inference
 app = Flask(__name__)
+
 @app.route("/inference", methods=["GET","POST"])
 def inference():
     try:
@@ -15,7 +16,7 @@ def inference():
         request_cnt = int(params.get("samples", "1"))
         request_cnt = min(8, request_cnt)
         data = {}
-        data.update({'candidates': model_inference.inference(sentence, request_cnt) if request_cnt > 0 else []})
+        data.update({'candidates': model_inference.inference(sentence, request_cnt) if request_cnt > 0 else []}, "/opt/app/BibleLM/Bible_model_ckpts")
         data.update({"success": True})
     except Exception as e:
         data = {"success": False, 'error' : str(e)}
