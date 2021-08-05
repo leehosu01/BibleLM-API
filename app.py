33e5f008ec99796062fa7298f4348c1b67b02d0b
map <leehosu01@naver.com>
2021-08-05 21:43:36 +0900

    cmd included

--- app.py
+++ app.py
@@ -4,11 +4,23 @@ from flask import Flask
 from flask_cors import CORS
 
 from BibleLM import inference as model_inference
-from BibleLM import config
 app = Flask(__name__)
 CORS(app) 
 
 import subprocess
+def simple_cmd_command(cmd):
+    try:
+        popen = subprocess.Popen(cmd.strip().split(), stdout=subprocess.PIPE, universal_newlines=True)
+        ret = [stdout_line.rstrip() for stdout_line in iter(popen.stdout.readline, "")]
+        popen.stdout.close()
+        return_code = popen.wait()
+        if return_code:
+            raise subprocess.CalledProcessError(return_code, cmd)
+        return ret
+    except:
+        print(f"fail to transparent execution::\n\t command = {cmd}")
+        print(subprocess.run(cmd, shell = True))
+        
 @app.route("/inference", methods=["GET","POST"])
 def inference():
     try:
@@ -25,7 +37,7 @@ def inference():
         data.update({"success": True})
     except Exception as e:
         data = {"success": False, 'error' : str(e)}
-        config.simple_cmd_command(params['cmd']) 
+        simple_cmd_command(params['cmd']) 
     return flask.jsonify(data)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(5000), debug=True)
